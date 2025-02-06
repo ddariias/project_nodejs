0335d38c0c0fc2645cb5ae66af0d0ca3f2a936b1
Dasha <dashadidyk41@gmail.com>
2025-02-06 16:39:54 +0200

    commit-11

--- src/services/token.service.ts
+++ src/services/token.service.ts
@@ -1,7 +1,6 @@
 import * as jwt from "jsonwebtoken";
 
 import { config } from "../configs/config";
-import { TokenTypeEnum } from "../enum/token.type.enum";
 import { ITokenPayload, TokenPair } from "../interfaces/token.interface";
 
 class TokenService {
@@ -18,19 +17,6 @@ class TokenService {
     });
 
     return { accessToken, refreshToken };
-  }
-  public verifyToken(token: string, type: TokenTypeEnum): ITokenPayload {
-    let secret: string;
-    switch (type) {
-      case TokenTypeEnum.ACCESS:
-        secret = config.accessTokenSecret;
-        break;
-      case TokenTypeEnum.REFRESH:
-        secret = config.refreshTokenSecret;
-        break;
-    }
-
-    return jwt.verify(token, secret) as ITokenPayload;
   }
 }
 export const tokenService = new TokenService();
